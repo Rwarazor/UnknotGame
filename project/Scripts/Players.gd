bab802f9aaedc431a251bee3917cd1a28bfb7842
Георгий Асатрян <ago-2004@mail.ru>
2024-03-24 03:26:53 +0300

    Improve interface

--- project/Scripts/Players.gd
+++ project/Scripts/Players.gd
@@ -3,16 +3,20 @@ extends Control
 # Пока что все кнопки ведут на одну и ту же сцену
 # Игра с ботом
 func _on_one_player_pressed():
+	Global.players = 1
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
 
 
 func _on_two_players_pressed():
+	Global.players = 2
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
 
 
 func _on_three_players_pressed():
+	Global.players = 3
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
 
 
 func _on_four_players_pressed():
+	Global.players = 4
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
