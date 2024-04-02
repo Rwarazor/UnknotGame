7c64d8c5ed8391b0ec5232da987bd285b78ebfef
Anastasiia Romanyuk <aaromanyuk@edu.hse.ru>
2024-04-02 17:42:27 +0300

    Settings created

--- project/Scenes/settings.gd
+++ project/Scenes/settings.gd
@@ -1,7 +1,26 @@
 extends Control
 
 @onready var button_click_sound = $MarginContainer/ButtonClickSound
+@onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
+@onready var MUSIC_BUS_ID = AudioServer.get_bus_index("Music")
+
 func _on_button_pressed():
 	button_click_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn") 
+
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(MUSIC_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(MUSIC_BUS_ID, value < .05)
+
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(SFX_BUS_ID, value < .05)
+
+
+func _on_repo_button_pressed():
+	button_click_sound.play()
+	await get_tree().create_timer(0.15).timeout
+	OS.shell_open("https://github.com/Rwarazor/UnknotGame")
