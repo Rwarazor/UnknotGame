9e8fa733c20bfeea85b4a1e0d90c36eada5245d1
Anastasiia Romanyuk <aaromanyuk@edu.hse.ru>
2024-04-04 23:49:34 +0300

    Escape menu improved

--- project/Scripts/game.gd
+++ project/Scripts/game.gd
@@ -42,7 +42,7 @@ func _refresh_state():
 func _on_back_to_menu_button_pressed():
 	button_click_sound.play()
 	await get_tree().create_timer(0.15).timeout
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	get_node("CanvasLayer/PausedMenu")._is_paused = !get_node("CanvasLayer/PausedMenu")._is_paused
 	
 func _on_edge_button_pressed():
 	button_click_sound.play()
--- project/paused_menu.gd
+++ project/paused_menu.gd
@@ -12,21 +12,16 @@ func set_paused(value:bool) -> void:
 	_is_paused = value
 	get_tree().paused = _is_paused
 	visible = _is_paused
-	
-
 
 func _on_resume_button_pressed():
 	button_click_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	_is_paused = false
 
-
 func _on_menu_button_pressed():
 	button_click_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
-
-
 
 func _on_quit_button_pressed():
 	button_click_sound.play()
