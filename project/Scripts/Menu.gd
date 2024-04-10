d390c8590858000b3ae2b49f1c826c88af759828
Георгий Асатрян <ago-2004@mail.ru>
2024-04-10 15:38:57 +0300

    Fix neutral component

--- project/Scripts/Menu.gd
+++ project/Scripts/Menu.gd
@@ -24,10 +24,7 @@ func _on_settings_button_pressed():
 	button_click_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
-	
+
 # Выход из игры
 func _on_exit_button_pressed():
 	get_tree().quit()
-
-
-
--- project/Scripts/Players.gd
+++ project/Scripts/Players.gd
@@ -4,6 +4,7 @@ extends Control
 
 func _on_one_player_pressed():
 	Global.players = 1
+	Global.virtual_players = 1
 	choose_players_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
@@ -11,18 +12,21 @@ func _on_one_player_pressed():
 
 func _on_two_players_pressed():
 	Global.players = 2
+	Global.virtual_players = 1
 	choose_players_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
 
 func _on_three_players_pressed():
 	Global.players = 3
+	Global.virtual_players = 0
 	choose_players_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
 
 func _on_four_players_pressed():
 	Global.players = 4
+	Global.virtual_players = 0
 	choose_players_sound.play()
 	await get_tree().create_timer(0.15).timeout
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") 
